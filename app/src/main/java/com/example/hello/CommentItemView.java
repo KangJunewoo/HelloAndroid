f434960b5c61bc0ffd0072c96dc3c6f0bc47ebbe
KangJunewoo <brianjune.dev@gmail.com>
2020-07-27 16:25:17 +0900

    [UPDATE] 2장 뼈대코드(미완) + 기본제공파일 추가

---
+++ app/src/main/java/com/example/hello/CommentItemView.java
@@ -0,0 +1,29 @@
+package com.example.hello;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class CommentItemView extends LinearLayout{
+
+    public CommentItemView(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public CommentItemView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    private void init(Context context){
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // FIXME : 왜 comment_item_view 불러오기가 안될까?
+        inflater.inflate(R.layout.comment_item_view, this, true);
+    }
+}
--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -4,16 +4,70 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 클래스 안에서 언제든 접근할 수 있도록 하려면 앞쪽에 선언.
+    TextView likeCountView;
+    Button likeButton;
+
+    int likeCount = 1;
+    boolean likeState = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // 인플레이션 과정, 이후 findViewById 사용 가능.
+
+
+        // 상관없다면 여기서 선언.
+        // Button likeButton = (Button) findViewById(R.id.likeButton);
+
+        likeButton = (Button) findViewById(R.id.likeButton);
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(likeState){
+                    decrLikeCount();
+                } else{
+                    incrLikeCount();
+                }
+
+                likeState = !likeState;
+            }
+        });
+
+        likeCountView = (TextView) findViewById(R.id.likeCountView);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        CommentAdapter adapter = new CommentAdapter();
+        listView.setAdapter(adapter);
+    }
+
+    class CommentAdapter extends BaseAdapter{
+
+    }
+
+    public void incrLikeCount(){
+        likeCount += 1;
+        likeCountView.setText(String.valueOf(likeCount));
+
+        likeButton.setBackgroundResource(R.drawable.ic_thumb_up_selected);
+
+    }
+
+    public void decrLikeCount(){
+        likeCount -= 1;
+        likeCountView.setText(String.valueOf(likeCount));
+
+        likeButton.setBackgroundResource(R.drawable.thumbs_up_selector);
     }
 
     public void onButton1Clicked(View v){
